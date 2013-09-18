6430971160dc7b478e404065d8f02a2f8e1895e9
Mehrdad Nourollahzadeh <mehrdad@mrtlsmr.cpsc.ucalgary.ca>
2013-09-18 14:52:04 -0600

    new method added to heuristic manager to allow heuristic filtering

--- src/ca/ucalgary/cpsc/ase/common/heuristic/HeuristicManager.java
+++ src/ca/ucalgary/cpsc/ase/common/heuristic/HeuristicManager.java
@@ -6,14 +6,14 @@ import java.util.Set;
 
 import javax.ejb.Remote;
 
-import ca.ucalgary.cpsc.ase.common.entity.Invocation;
-import ca.ucalgary.cpsc.ase.common.entity.Method;
 import ca.ucalgary.cpsc.ase.common.query.Query;
 
 @Remote
 public interface HeuristicManager {
 
 	public Map<Integer, VotingResult> match(Query q) throws Exception;
+	
+	public Map<Integer, VotingResult> match(Query q, Set<String> filter) throws Exception;
 
 	public List getMatchingItems(Integer id, Query q,
 			String heuristic);
